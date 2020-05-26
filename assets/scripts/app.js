47527e32920b7a00058f80711a358cb360984991
njtan142 <njtan142@penguin>
2020-05-26 17:24:18 +0800

    Initial Commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,104 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "rapping",
+				weight: 12.3
+			}, {
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "python",
+				weight: 10
+			}, {
+				text: "java",
+				weight: 9
+			}, {
+				text: "qazi",
+				weight: 15
+			}, {
+				text: "newText",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Front-End Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "white" , "background-color" : "crimson"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("profile-photo");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
